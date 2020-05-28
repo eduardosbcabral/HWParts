960f81e39e60ee20f1745177306cac93b1f5d91e
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-05-27 23:57:45 -0300

    Add migration to storage and processor entities

--- HWParts.Core/Migrations/20200528023411_Create-Storage-Entity.Designer.cs
+++ HWParts.Core/Migrations/20200528023411_Create-Storage-Entity.Designer.cs
@@ -4,14 +4,16 @@ using HWParts.Core.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HWParts.Core.Migrations
 {
     [DbContext(typeof(HWPartsDbContext))]
-    partial class HWPartsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200528023411_Create-Storage-Entity")]
+    partial class CreateStorageEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
