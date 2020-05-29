9669581164e34a6aaf5b8c14c609712b18462ac2
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-05-28 23:16:17 -0300

    Create Case Repository Interface

---
+++ HWParts.Core/Domain/Interfaces/ICaseRepository.cs
@@ -0,0 +1,14 @@
+using HWParts.Core.Application.ViewModels.Case;
+using HWParts.Core.Domain.Entities;
+using HWParts.Core.Infrastructure.Common.Pagination;
+using System;
+
+namespace HWParts.Core.Domain.Interfaces
+{
+    public interface ICaseRepository : IRepository<Case>
+    {
+        Case GetByPlatformId(string platformId);
+        PaginationObject<CaseViewModel> ListPaginated(int? page);
+        bool Exists(Guid id);
+    }
+}
