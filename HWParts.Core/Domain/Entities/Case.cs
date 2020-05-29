c5a897eacd8a8f00e9efdba45f9c7cb4414d1cee
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-05-28 23:01:14 -0300

    Create case entity

---
+++ HWParts.Core/Domain/Entities/Case.cs
@@ -0,0 +1,37 @@
+using HWParts.Core.Domain.Enums;
+using System;
+
+namespace HWParts.Core.Domain.Entities
+{
+    public class Case : ComponentBase
+    {
+        public Case(
+            string brand,
+            string model,
+            string platformId,
+            string imageUrl,
+            string url,
+            EPlatform platform)
+            : base(brand, model, platformId, imageUrl, url, platform)
+        {
+        }
+
+        public void Update(
+            string platformId,
+            string imageUrl,
+            string url,
+            EPlatform platform,
+            string brand,
+            string model)
+        {
+            PlatformId = platformId;
+            ImageUrl = imageUrl;
+            Url = url;
+            Platform = platform;
+            Brand = brand;
+            Model = model;
+
+            UpdatedAt = DateTime.Now;
+        }
+    }
+}
