1be510b64b1d51afcc284686f15bd54c47c3e799
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-05-29 22:48:12 -0300

    Create User View Models

---
+++ HWParts.Core/Infrastructure/Identity/Models/UserLogin.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HWParts.Core.Infrastructure.Identity.Models
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em um formato inválido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O campo {0} deve conter entre {2} e {1} caracteres..", MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
---
+++ HWParts.Core/Infrastructure/Identity/Models/UserRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace HWParts.Core.Infrastructure.Identity.Models
+{
+    class UserRegistration
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em um formato inválido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O campo {0} deve conter entre {2} e {1} caracteres..", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "A confirmação da senha não é igual.")]
+        public string PasswordConfirm { get; set; }
+    }
+}
