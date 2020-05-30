e660d8d51915961a8bf641ffa04e45324333b059
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-05-29 22:49:32 -0300

    Implement Application Claims

---
+++ HWParts.Core/Infrastructure/Identity/Authorization/ClaimRequirement.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace HWParts.Core.Infrastructure.Identity.Authorization
+{
+    public class ClaimRequirement : IAuthorizationRequirement
+    {
+        public string ClaimName { get; set; }
+        public string ClaimValue { get; set; }
+
+        public ClaimRequirement(string claimName, string claimValue)
+        {
+            ClaimName = claimName;
+            ClaimValue = claimValue;
+        }
+    }
+}
