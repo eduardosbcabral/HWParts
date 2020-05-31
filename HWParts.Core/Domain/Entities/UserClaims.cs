836c3a7afce5d80e9ae370983f0b21c3cefd7698
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-05-30 23:22:12 -0300

    Create UserClaims static entity

---
+++ HWParts.Core/Domain/Entities/UserClaims.cs
@@ -0,0 +1,13 @@
+﻿namespace HWParts.Core.Domain.Entities
+{
+    public static class UserClaims
+    {
+        public static string Components => "Components";
+    }
+
+    public static class UserClaimValues
+    {
+        public static string Write => "Write";
+        public static string Read => "Read";
+    }
+}
