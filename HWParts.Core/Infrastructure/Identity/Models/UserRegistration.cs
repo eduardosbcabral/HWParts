bca067a596f00ea39bdec12f8528321381a8bff0
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-05-30 23:19:28 -0300

    Add and update data annotations in UserRegistration API model

--- HWParts.Core/Infrastructure/Identity/Models/UserRegistration.cs
+++ HWParts.Core/Infrastructure/Identity/Models/UserRegistration.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace HWParts.Core.Infrastructure.Identity.Models
 {
@@ -9,13 +6,18 @@ namespace HWParts.Core.Infrastructure.Identity.Models
     {
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         [EmailAddress(ErrorMessage = "O campo {0} está em um formato inválido.")]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         [StringLength(100, ErrorMessage = "O campo {0} deve conter entre {2} e {1} caracteres..", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
         public string Password { get; set; }
 
         [Compare("Password", ErrorMessage = "A confirmação da senha não é igual.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar Senha")]
         public string PasswordConfirm { get; set; }
     }
 }
