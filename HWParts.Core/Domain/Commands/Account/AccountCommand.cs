381a2253e4147c4062f81b99aa3218147101a930
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-05 23:50:25 -0300

    Create register account command

---
+++ HWParts.Core/Domain/Commands/Account/AccountCommand.cs
@@ -0,0 +1,11 @@
+﻿using HWParts.Core.Domain.Core.Commands;
+
+namespace HWParts.Core.Domain.Commands
+{
+    public abstract class AccountCommand : Command
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ HWParts.Core/Domain/Commands/Account/RegisterAccountCommand.cs
@@ -0,0 +1,13 @@
+﻿using HWParts.Core.Domain.Validations;
+
+namespace HWParts.Core.Domain.Commands
+{
+    public class RegisterAccountCommand : AccountCommand
+    {
+        public override bool IsValid()
+        {
+            ValidationResult = new RegisterAccountCommandValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
