3ce6be332defad4c7a8ca1e499701a86ad16455d
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-05 23:52:32 -0300

    Create account app service

---
+++ HWParts.Core/Application/Interfaces/IAccountAppService.cs
@@ -0,0 +1,9 @@
+﻿using HWParts.Core.Application.ViewModels.Account;
+
+namespace HWParts.Core.Application.Interfaces
+{
+    public interface IAccountAppService
+    {
+        void Register(RegisterAccountViewModel registerAccountViewModel);
+    }
+}
---
+++ HWParts.Core/Application/Services/AccountAppService.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HWParts.Core.Application.Interfaces;
+using HWParts.Core.Application.ViewModels.Account;
+using HWParts.Core.Domain.Commands;
+using HWParts.Core.Domain.Core.Bus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HWParts.Core.Application.Services
+{
+    public class AccountAppService : IAccountAppService
+    {
+        private readonly IMapper _mapper;
+        private readonly IMediatorHandler Bus;
+
+        public AccountAppService(
+            IMapper mapper,
+            IMediatorHandler mediatorHandler)
+        {
+            _mapper = mapper;
+            Bus = mediatorHandler;
+        }
+        public void Register(RegisterAccountViewModel registerAccountViewModel)
+        {
+            var command = _mapper.Map<RegisterAccountCommand>(registerAccountViewModel);
+            Bus.SendCommand(command);
+        }
+    }
+}
