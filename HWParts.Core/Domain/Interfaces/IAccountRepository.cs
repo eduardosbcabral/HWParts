e916cd968b2a8ed9d1a295aaa20ec13c4f055a26
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-08 20:44:04 -0300

    Create Account Repository

---
+++ HWParts.Core/Domain/Interfaces/IAccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using HWParts.Core.Application.ViewModels.Account;
+using HWParts.Core.Infrastructure.Common.Pagination;
+
+namespace HWParts.Core.Domain.Interfaces
+{
+    public interface IAccountRepository
+    {
+        PaginationObject<AccountViewModel> ListPaginated(int? page);
+    }
+}
