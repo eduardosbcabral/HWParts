32402f54bc78aa798f63af3fe1be2e03bb87d151
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-08 20:45:12 -0300

    Create Admin Account controller and views

---
+++ HWParts.Web/Controllers/Admin/AdminAccountController.cs
@@ -0,0 +1,30 @@
+﻿using HWParts.Core.Application.Interfaces;
+using HWParts.Core.Domain.Core.Notifications;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HWParts.Web.Controllers.Admin
+{
+    [Authorize]
+    [Route("admin/account")]
+    public class AdminAccountController : BaseController
+    {
+        private readonly IAccountAppService _accountAppService;
+
+        public AdminAccountController(
+            INotificationHandler<DomainNotification> notifications,
+            IAccountAppService accountAppService)
+            : base(notifications)
+        {
+            _accountAppService = accountAppService;
+        }
+
+        [HttpGet("list")]
+        public IActionResult Index(int? page)
+        {
+            var paginationObject = _accountAppService.ListPaginated(page);
+            return View(paginationObject);
+        }
+    }
+}
