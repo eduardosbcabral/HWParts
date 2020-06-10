bea616d8728ebd81d72c072b23daae5f7f5d528f
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-10 02:58:52 -0300

    Create ForgotPasswordAccountCommand class

---
+++ HWParts.Core/Domain/Commands/Account/ForgotPasswordAccountCommand.cs
@@ -0,0 +1,13 @@
+﻿using HWParts.Core.Domain.Validations;
+
+namespace HWParts.Core.Domain.Commands
+{
+    public class ForgotPasswordAccountCommand : AccountCommand
+    {
+        public override bool IsValid()
+        {
+            ValidationResult = new ForgotPasswordAccountCommandValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
