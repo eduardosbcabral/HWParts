ee61a7760e21c0952a19bbd02395f84f366d8fda
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-15 03:22:03 -0300

    Create ImportCasesCommand and validator

---
+++ HWParts.Core/Domain/Commands/Case/ImportCasesCommand.cs
@@ -0,0 +1,22 @@
+﻿using HWParts.Core.Domain.Core.Commands;
+using HWParts.Core.Domain.Validations;
+using Microsoft.AspNetCore.Http;
+
+namespace HWParts.Core.Domain.Commands
+{
+    public class ImportCasesCommand : Command
+    {
+        public IFormFile File { get; set; }
+
+        public ImportCasesCommand()
+        {
+
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new ImportCaseCommandValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
