1a45e2ec1ac171349ba61f53a24bbb869781b6cf
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-15 03:21:24 -0300

    Create ImportCasesViewModel

---
+++ HWParts.Core/Application/ViewModels/Case/ImportCasesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace HWParts.Core.Application.ViewModels.Case
+{
+    public class ImportCasesViewModel
+    {
+        [Required]
+        public IFormFile File { get; set; }
+    }
+}
