6042f9ef9c3434bba845035a5fddc2a8db7f6c3d
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-14 23:15:38 -0300

    Create GraphicsCardController

---
+++ HWParts.Web/Controllers/Components/GraphicsCardController.cs
@@ -0,0 +1,50 @@
+﻿using HWParts.Core.Application.Interfaces;
+using HWParts.Core.Domain.Core.Notifications;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+
+namespace HWParts.Web.Controllers.Components
+{
+    [Route("component/graphics-card")]
+    [AllowAnonymous]
+    public class GraphicsCardController : BaseController
+    {
+        private readonly IGraphicsCardAppService _appService;
+
+        public GraphicsCardController(
+            INotificationHandler<DomainNotification> notifications,
+            IGraphicsCardAppService appService)
+            : base(notifications)
+        {
+            _appService = appService;
+        }
+
+        [HttpGet]
+        public IActionResult Index(int? page)
+        {
+            var paginationObject = _appService.ListPaginated(page);
+            return View(paginationObject);
+        }
+
+        [HttpGet("detail")]
+        public IActionResult Detail(Guid? id)
+        {
+            if (id is null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = _appService.GetById(id.Value);
+
+            if (viewModel is null)
+            {
+                return NotFound();
+            }
+
+            return View(viewModel);
+        }
+    }
+}
