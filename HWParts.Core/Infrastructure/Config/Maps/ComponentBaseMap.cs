5f393ea621537050bbf5f7bdfb0666051b89bba2
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-17 02:02:03 -0300

    Create ComponentBaseMap

---
+++ HWParts.Core/Infrastructure/Config/Maps/ComponentBaseMap.cs
@@ -0,0 +1,31 @@
+﻿using HWParts.Core.Domain.Entities;
+using HWParts.Core.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HWParts.Core.Infrastructure.Config.Maps
+{
+    public class ComponentBaseMap : IEntityTypeConfiguration<ComponentBase>
+    {
+        public void Configure(EntityTypeBuilder<ComponentBase> builder)
+        {
+            builder.ToTable("TB_COMPONENTS");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.CreatedAt)
+                .IsRequired();
+
+            builder.Property(x => x.UpdatedAt)
+                .IsRequired(false);
+
+            builder
+                .Property(e => e.Platform)
+                .HasConversion(
+                    v => v.ToString(),
+                    v => (EPlatform)Enum.Parse(typeof(EPlatform), v));
+        }
+    }
+}
