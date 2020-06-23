3c8f5afff2febf1ad0e3add56b36471a2618db25
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-22 21:57:37 -0300

    Create ComponentPrice entity

---
+++ HWParts.Core/Domain/Entities/ComponentPrice.cs
@@ -0,0 +1,25 @@
+﻿using HWParts.Core.Domain.Core.Entities;
+using HWParts.Core.Domain.Enums;
+
+namespace HWParts.Core.Domain.Entities
+{
+    public class ComponentPrice : EntityBase
+    {
+        public decimal Price { get; private set; }
+        public EPlatform Platform { get; private set; }
+        public ComponentBase Component { get; private set; }
+
+        public ComponentPrice()
+        {
+
+        }
+
+        public ComponentPrice(decimal price, EPlatform platform, ComponentBase component)
+            : base()
+        {
+            Price = price;
+            Platform = platform;
+            Component = component;
+        }
+    }
+}
