209326d6de7dfe37972622f308dd795c82609c65
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-23 22:44:32 -0300

    Create ComponentPrice controller

---
+++ HWParts.Web/Controllers/ComponentPriceController.cs
@@ -0,0 +1,39 @@
+﻿using HWParts.Core.Application.Interfaces;
+using HWParts.Core.Application.Services;
+using HWParts.Core.Domain.Core.Notifications;
+using HWParts.Core.Domain.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace HWParts.Web.Controllers
+{
+    [Authorize]
+    [Route("admin/{case|graphics-card|memory|motherboard|power-supply|processor|storage}/edit/{id:guid}/price")]
+    public class ComponentPriceController : BaseController
+    {
+        private readonly IComponentPriceAppService _appService;
+
+        public ComponentPriceController(
+            INotificationHandler<DomainNotification> notifications,
+            IComponentPriceAppService appService) 
+            : base(notifications)
+        {
+            _appService = appService;
+        }
+
+        [HttpGet]
+        public IActionResult Index(Guid id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var prices = _appService.GetAllPricesByComponentId(id);
+
+            return View(prices);
+        }
+    }
+}
