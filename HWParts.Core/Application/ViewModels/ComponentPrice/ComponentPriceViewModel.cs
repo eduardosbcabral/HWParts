921f5f3a0d2f3a93e2230bec015b290973fe02ba
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-23 22:47:04 -0300

    Create ComponentPrice view model

---
+++ HWParts.Core/Application/ViewModels/ComponentPrice/ComponentPriceViewModel.cs
@@ -0,0 +1,15 @@
+﻿using HWParts.Core.Domain.Enums;
+
+namespace HWParts.Core.Application.ViewModels.ComponentPrice
+{
+    public class ComponentPriceViewModel
+    {
+        public decimal Price { get; set; }
+        public EPlatform Platform { get; set; }
+
+        public ComponentPriceViewModel()
+        {
+
+        }
+    }
+}
