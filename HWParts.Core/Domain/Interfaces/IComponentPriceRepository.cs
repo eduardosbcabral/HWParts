75f335c67a1244504c290b7e7d62dc538bf13bf0
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-23 22:45:18 -0300

    Create ComponentPrice repository

---
+++ HWParts.Core/Domain/Interfaces/IComponentPriceRepository.cs
@@ -0,0 +1,11 @@
+﻿using HWParts.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace HWParts.Core.Domain.Interfaces
+{
+    public interface IComponentPriceRepository : IRepository<ComponentPrice>
+    {
+        IList<ComponentPrice> GetAllByComponentId(Guid componentId);
+    }
+}
---
+++ HWParts.Core/Infrastructure/Repositories/ComponentPriceRepository.cs
@@ -0,0 +1,30 @@
+﻿using HWParts.Core.Domain.Entities;
+using HWParts.Core.Domain.Interfaces;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HWParts.Core.Infrastructure.Repositories
+{
+    public class ComponentPriceRepository : Repository<ComponentPrice>, IComponentPriceRepository
+    {
+        public ComponentPriceRepository(HWPartsDbContext context) 
+            : base(context)
+        {
+        }
+
+        public IList<ComponentPrice> GetAllByComponentId(Guid componentId)
+        {
+            return DbSet
+                .Where(x => x.Component.Id == componentId)
+                .Select(x => new ComponentPrice(
+                    x.Id,
+                    x.Price,
+                    x.Platform)
+                )
+                .ToList();
+        }
+    }
+}
