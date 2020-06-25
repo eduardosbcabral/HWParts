097889a8f176de048dab60d0fae4f4c71060d175
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-24 23:16:29 -0300

    Update ComponentPriceController class

--- HWParts.Web/Controllers/ComponentPriceController.cs
+++ HWParts.Web/Controllers/ComponentPriceController.cs
@@ -1,7 +1,6 @@
 ﻿using HWParts.Core.Application.Interfaces;
-using HWParts.Core.Application.Services;
+using HWParts.Core.Application.ViewModels.ComponentPrice;
 using HWParts.Core.Domain.Core.Notifications;
-using HWParts.Core.Domain.Interfaces;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -33,7 +32,37 @@ namespace HWParts.Web.Controllers
 
             var prices = _appService.GetAllPricesByComponentId(id);
 
+            ViewData["ReturnUrl"] = PreviousUrl();
+
             return View(prices);
+        }
+
+        [HttpGet("create")]
+        public IActionResult Create()
+        {
+            ViewData["ReturnUrl"] = PreviousUrl();
+            return View();
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create(ComponentPriceViewModel viewModel)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        private string PreviousUrl()
+        {
+            var currentUrl = Request.Path.Value;
+            var splittedUrl = currentUrl.Split("/");
+
+            var previousUrl = string.Empty;
+
+            for(var i = 1; i < splittedUrl.Length-1; i++)
+            {
+                previousUrl += "/" + splittedUrl[i];
+            }
+
+            return previousUrl;
         }
     }
 }
