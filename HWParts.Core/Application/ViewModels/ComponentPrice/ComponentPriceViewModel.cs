193c5b7e1e2948bc8ba742c9de5936f0811ce6f9
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-24 23:14:53 -0300

    Create ComponentPriceViewModel class

--- HWParts.Core/Application/ViewModels/ComponentPrice/ComponentPriceViewModel.cs
+++ HWParts.Core/Application/ViewModels/ComponentPrice/ComponentPriceViewModel.cs
@@ -1,15 +1,21 @@
 ﻿using HWParts.Core.Domain.Enums;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace HWParts.Core.Application.ViewModels.ComponentPrice
 {
     public class ComponentPriceViewModel
     {
+        [Required(ErrorMessage = "Preço é obrigatório.")]
+        [DisplayName("Preço")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Plataforma é obrigatória.")]
+        [DisplayName("Plataforma")]
         public EPlatform Platform { get; set; }
 
         public ComponentPriceViewModel()
         {
-
         }
     }
 }
