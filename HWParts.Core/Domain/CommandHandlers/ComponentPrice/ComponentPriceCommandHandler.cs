c4f12bbbfe7c2b73a32c5f42a26f9b0fd37daf5c
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-26 01:10:17 -0300

    Finish create component price register command

--- HWParts.Core/Domain/CommandHandlers/ComponentPrice/ComponentPriceCommandHandler.cs
+++ HWParts.Core/Domain/CommandHandlers/ComponentPrice/ComponentPriceCommandHandler.cs
@@ -2,6 +2,7 @@
 using HWParts.Core.Domain.Core.Bus;
 using HWParts.Core.Domain.Core.Notifications;
 using HWParts.Core.Domain.Entities;
+using HWParts.Core.Domain.Events;
 using HWParts.Core.Domain.Interfaces;
 using MediatR;
 using System.ComponentModel;
@@ -15,16 +16,19 @@ namespace HWParts.Core.Domain.CommandHandlers
     {
         private readonly IMediatorHandler Bus;
         private readonly IComponentPriceRepository _componentPriceRepository;
+        private readonly IComponentBaseRepository _componentBaseRepository;
 
         public ComponentPriceCommandHandler(
             IUnitOfWork uow,
             IMediatorHandler bus,
             INotificationHandler<DomainNotification> notifications,
-            IComponentPriceRepository componentPriceRepository) 
+            IComponentPriceRepository componentPriceRepository,
+            IComponentBaseRepository componentBaseRepository) 
             : base(uow, bus, notifications)
         {
             Bus = bus;
             _componentPriceRepository = componentPriceRepository;
+            _componentBaseRepository = componentBaseRepository;
         }
 
         public Task<bool> Handle(RegisterComponentPriceCommand request, CancellationToken cancellationToken)
@@ -35,12 +39,25 @@ namespace HWParts.Core.Domain.CommandHandlers
                 return Task.FromResult(false);
             }
 
-            var component = new ComponentBase(request.ComponentBaseId);
+            var component = _componentBaseRepository.GetById(request.ComponentBaseId);
 
             var componentPrice = new ComponentPrice(
                 request.Price,
                 request.Platform,
                 component);
+
+            if (_componentPriceRepository.AlreadyRegisteredOnPlatform(component.Id, componentPrice.Platform))
+            {
+                Bus.RaiseEvent(new DomainNotification(request.MessageType, "Preço do componente já foi registrado na plataforma."));
+                return Task.FromResult(false);
+            }
+
+            _componentPriceRepository.Add(componentPrice);
+
+            if (Commit())
+            {
+                Bus.RaiseEvent(new ComponentPriceRegisteredEvent());
+            }
 
             return Task.FromResult(true);
         }
