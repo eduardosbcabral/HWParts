b607f3486cc65bc25995b4a712a5de8dbf761b26
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-25 23:49:07 -0300

    Create component price command classes

---
+++ HWParts.Core/Domain/Commands/ComponentPrice/ComponentPriceCommand.cs
@@ -0,0 +1,16 @@
+﻿using HWParts.Core.Domain.Core.Commands;
+using HWParts.Core.Domain.Enums;
+using System;
+
+namespace HWParts.Core.Domain.Commands
+{
+    public abstract class ComponentPriceCommand : Command
+    {
+        public Guid Id { get; set; }
+
+        public decimal Price { get; set; }
+        public EPlatform Platform { get; set; }
+
+        public Guid ComponentBaseId { get; set; }
+    }
+}
---
+++ HWParts.Core/Domain/Commands/ComponentPrice/RegisterComponentPriceCommand.cs
@@ -0,0 +1,18 @@
+﻿using HWParts.Core.Domain.Validations;
+
+namespace HWParts.Core.Domain.Commands
+{
+    public class RegisterComponentPriceCommand : ComponentPriceCommand
+    {
+        public RegisterComponentPriceCommand()
+        {
+
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new RegisterComponentPriceCommandValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
