4c8f0d3b15a390df24c51141ceefd33452b5c4ca
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-25 23:49:25 -0300

    Create component price command validation classes

---
+++ HWParts.Core/Domain/Validations/ComponentPrice/ComponentPriceValidation.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using HWParts.Core.Domain.Commands;
+
+namespace HWParts.Core.Domain.Validations
+{
+    public class ComponentPriceValidation<T> : AbstractValidator<T> where T : ComponentPriceCommand
+    {
+        public void ValidatePrice()
+        {
+            RuleFor(c => c.Price)
+                .NotEmpty().WithMessage("O preço é obrigatório.");
+        }
+
+        public void ValidatePlatform()
+        {
+            RuleFor(c => c.Platform)
+                .NotEmpty().WithMessage("A plataforma é obrigatória.");
+        }
+    }
+}
---
+++ HWParts.Core/Domain/Validations/ComponentPrice/RegisterComponentPriceCommandValidation.cs
@@ -0,0 +1,13 @@
+﻿using HWParts.Core.Domain.Commands;
+
+namespace HWParts.Core.Domain.Validations
+{
+    public class RegisterComponentPriceCommandValidation : ComponentPriceValidation<RegisterComponentPriceCommand>
+    {
+        public RegisterComponentPriceCommandValidation()
+        {
+            ValidatePrice();
+            ValidatePlatform();
+        }
+    }
+}
