e5f6465cf9bec510223614950b71f2b8b23dd015
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-28 05:49:21 -0300

    Create UpdateComponentPriceCommand class

---
+++ HWParts.Core/Domain/Commands/ComponentPrice/UpdateComponentPriceCommand.cs
@@ -0,0 +1,18 @@
+﻿using HWParts.Core.Domain.Validations;
+
+namespace HWParts.Core.Domain.Commands
+{
+    public class UpdateComponentPriceCommand : ComponentPriceCommand
+    {
+        public UpdateComponentPriceCommand()
+        {
+
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new UpdateComponentPriceCommandValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
