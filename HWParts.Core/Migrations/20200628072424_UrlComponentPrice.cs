c137e49dad2636508a0633b00a6447015d04ad8c
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-28 05:50:45 -0300

    Add migrations

---
+++ HWParts.Core/Migrations/20200628072424_UrlComponentPrice.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HWParts.Core.Migrations
+{
+    public partial class UrlComponentPrice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "TB_COMPONENTS_PRICES",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "TB_COMPONENTS_PRICES");
+        }
+    }
+}
