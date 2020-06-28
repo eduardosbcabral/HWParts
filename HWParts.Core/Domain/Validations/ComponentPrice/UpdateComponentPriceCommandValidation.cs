bcd1b76005931ca249d17657216d3ff8c4ac3e86
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-28 05:50:28 -0300

    Create update component price command validation class

---
+++ HWParts.Core/Domain/Validations/ComponentPrice/UpdateComponentPriceCommandValidation.cs
@@ -0,0 +1,13 @@
+﻿using HWParts.Core.Domain.Commands;
+
+namespace HWParts.Core.Domain.Validations
+{
+    public class UpdateComponentPriceCommandValidation : ComponentPriceValidation<UpdateComponentPriceCommand>
+    {
+        public UpdateComponentPriceCommandValidation()
+        {
+            ValidatePlatform();
+            ValidatePrice();
+        }
+    }
+}
