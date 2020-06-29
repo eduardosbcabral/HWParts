3c37d984bba9e753f42bff88d04f108b282aa0a3
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-29 04:00:54 -0300

    Create RemoveComponentPriceCommand class

---
+++ HWParts.Core/Domain/Commands/ComponentPrice/RemoveComponentPriceCommand.cs
@@ -0,0 +1,19 @@
+﻿using HWParts.Core.Domain.Validations;
+using System;
+
+namespace HWParts.Core.Domain.Commands
+{
+    public class RemoveComponentPriceCommand : ComponentPriceCommand
+    {
+        public RemoveComponentPriceCommand(Guid id)
+        {
+            Id = id;
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new RemoveComponentPriceCommandValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
