5f2cf7cd1099a48263815c801813d9892fa136e5
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-06-29 04:01:16 -0300

    Add validateId method to component price validation base class

--- HWParts.Core/Domain/Validations/ComponentPrice/ComponentPriceValidation.cs
+++ HWParts.Core/Domain/Validations/ComponentPrice/ComponentPriceValidation.cs
@@ -1,10 +1,17 @@
 ﻿using FluentValidation;
 using HWParts.Core.Domain.Commands;
+using System;
 
 namespace HWParts.Core.Domain.Validations
 {
     public class ComponentPriceValidation<T> : AbstractValidator<T> where T : ComponentPriceCommand
     {
+        protected void ValidateId()
+        {
+            RuleFor(c => c.Id)
+                .NotEqual(Guid.Empty);
+        }
+
         public void ValidatePrice()
         {
             RuleFor(c => c.Price)
