2748c03ac66bc69bef4aa819187a6be3d80e48e4
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-07-04 01:16:07 -0300

    Create ApplicationRoles model

---
+++ HWParts.Core/Infrastructure/Identity/Models/ApplicationRoles.cs
@@ -0,0 +1,13 @@
+﻿namespace HWParts.Core.Infrastructure.Identity.Models
+{
+    public static class ApplicationRoles
+    {
+        public const string Super = "Super";
+        public const string Admin = "Admin";
+        public const string Moderator = "Moderator";
+        public const string Common = "Common";
+
+        public const string AllRoles = Super + "," + Admin + "," + Moderator + "," + Common;
+        public const string StaffRoles = Super + "," + Admin + "," + Moderator;
+    }
+}
