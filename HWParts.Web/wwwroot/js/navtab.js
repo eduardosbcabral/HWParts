b6c9bac47feec5d129bb546807129366d82592aa
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-07-20 23:46:53 -0300

    Create navtab js file

---
+++ HWParts.Web/wwwroot/js/navtab.js
@@ -0,0 +1,7 @@
+﻿function changeCss() {
+    var navElement = document.querySelector(".hw-navtab-wrapper");
+    if (navElement) 
+        this.scrollY < 50 ? navElement.style.top = (50 - this.scrollY) + 'px' : navElement.style.top = '0px';
+}
+
+window.addEventListener("scroll", changeCss, false);
